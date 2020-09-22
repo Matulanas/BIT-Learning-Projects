eab4e3b653b2fe90de753bb7d74bddeba8d15aea
Matulanas <61939694+Matulanas@users.noreply.github.com>
2020-09-22 21:27:37 +0300

    First IF

---
+++ 7 JS Uzduotys/js/main.js
@@ -0,0 +1,13 @@
+const skaiciai = [22,1, 0, 66, 88];
+
+let rezultatas = 0;
+
+rezultatas = skaiciai[0] - skaiciai[1] + skaiciai[2] - skaiciai[3] + skaiciai[4];
+
+if (rezultatas >= 0) {
+    console.log('Teigiamas rezultatas: ' + rezultatas);
+} else {
+    console.log('Neigiamas rezultatas: ' + rezultatas);
+}
+
+
