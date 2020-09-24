f81006b3b2c65d94b5b07ab1aaf9f411b973d3f7
Matulanas <61939694+Matulanas@users.noreply.github.com>
2020-09-24 14:09:16 +0300

    .

--- 6 JS Intro/js/script.js
+++ 6 JS Intro/js/script.js
@@ -8,3 +8,113 @@
 // increment: ++ (before or after)
 // decrement: -- (before or after)
 // update: +=, -=, *=, /=
+
+
+const day = 'dev';
+
+switch (day) {
+    case 'pir':
+        console.log('1')
+        break;
+    case 'ant':
+        console.log('21')
+        break;
+    case 'tre':
+        console.log('3')
+        break;
+    case 'ket':
+        console.log('4')
+        break;
+    case 'pen':
+        console.log('5')
+        break;
+    case 'ses':
+        console.log('6')
+        break;
+    case 'sek':
+        console.log('7')
+        break;
+    default:
+        console.log('Nou nou')
+        break;
+}
+console.log('-----');
+
+const stop = 'geniu';
+
+switch (stop){
+    case 'Naujoji Vilnia':
+        console.log('Naujoji Vilnia');
+        break;
+    case 'Geniu':
+        console.log('Geniu');
+    case 'Rudens':
+        console.log('Rudens');
+    case 'Karaliaus Mindaugo':
+        console.log('Karaliaus Mindaugo');
+    case 'Zaliasis Tiltas':
+        console.log('Zaliasis Tiltas');
+        break;
+
+    case 'Lvovo':
+        console.log('Lvovo');
+    case 'Tuskulenu':
+        console.log('Tuskulenu');
+    case 'Vasaros':
+        console.log('Vasaros');
+    case 'Rudens2':
+        console.log('Rudens2');
+    case 'Tremtiniu':
+        console.log('Tremtiniu');
+
+    default:
+        console.log('Ar turi mabylka?');
+        break;
+}
+
+console.log('-----');
+console.log('-----');
+console.log('-----');
+console.log('-----');
+console.log("FOR");
+
+
+const sayBye = 'Ate';
+
+const count = 6;
+
+for (let i=0; i<count; i++) {
+    console.log(sayBye);
+}
+
+console.log('---');
+
+// 0 - 4    - 10
+// 0 - 100    - 5050
+// 574 - 815    - didelis teigiamas
+// -70 - 30     - mažas neigiamas
+// -50 - 50     - 0
+// 0 - 0    - 0
+
+const nuo = 0;
+const iki = 5;
+let sum = 0;
+for(let i=0; i <= 5; i++){
+    sum = sum + i;    
+}
+const rez = `Suma nuo ${nuo} iki ${iki} yra ${sum}`;
+console.log(rez);
+
+console.log('----');
+
+let sum2 = 0;
+for(let i=0; i<=100; i++){
+    sum2 = sum2 + i;
+}
+console.log(sum2);
+
+console.log('----');
+
+let sum3 = 0;
+
+// for(i = 574)
--- 7 JS Uzduotys/js/main.js
+++ 7 JS Uzduotys/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const skaiciai = [22,1, 0, 66, 88];
 console.log('Iš array list (' + skaiciai + ') padarius šias funkcijas: 1-2+3-4+5');
 let rezultatas = 0;
@@ -15,6 +17,7 @@ console.log('----------------');
 console.log('----------------');
 console.log('----------------');
 console.log('Skaičių kintamųjų palyginimas:');
+console.log('--');
 
 const pirmassk = 2;
 const antrassk = 2;
@@ -37,6 +40,7 @@ console.log('----------------');
 console.log('----------------');
 console.log('----------------');
 console.log('Tekstų kintamųjų palyginimas:');
+console.log('--');
 
 const pirmast = "asdfa";
 const antrast = "asdfg";
@@ -59,23 +63,141 @@ console.log('----------------');
 console.log('----------------');
 console.log('----------------');
 console.log('Tekstų kintamųjų palyginimas:');
+console.log('--');
 
-const sarasas = ['Kas', 'Kur', 'Kada'];
+const sarasas = ['aaa', 'aaa', 'aaa'];
+console.log(sarasas);
 console.log('Pirmas sąrašo kintamasis: (' + sarasas[0] + ') jo ilgis: ' + sarasas[0].length + ' simboliai');
 console.log('Antras sąrašo kintamasis: (' + sarasas[1] + ') jo ilgis: ' + sarasas[1].length + ' simboliai');
 console.log('Trečias sąrašo kintamasis: (' + sarasas[2] + ') jo ilgis: ' + sarasas[2].length + ' simboliai');
 
-if (sarasas[0].length == sarasas[1].length && sarasas[0].length == sarasas[2].length) {
+if (sarasas[0].length == sarasas[1].length && sarasas[0].length == sarasas[2].length && sarasas[1].length == sarasas[2].length) {
     console.log('Visi trys yra lygūs');
 } 
 else if(sarasas[0].length > sarasas[1].length && sarasas[0].length > sarasas[2].length) 
 {
     console.log('Pirmas ilgiausias');
 }
-else if(sarasas[0].length < sarasas[1].length && sarasas[1].length > sarasas[2].length){
+else  if(sarasas[0].length < sarasas[1].length && sarasas[1].length > sarasas[2].length){
     console.log('Antras ilgiausias');
 }
 else 
 {
     console.log('Trečias yra ilgiausias');
 }
+
+console.log('--');
+
+const nr1 = 1;
+const nr2 = 2;
+const nr3 = 3;
+
+if((nr1 + nr2) == nr3 && (1 + 2) == 3){
+    console.log('Pomadoras');
+} else {
+    console.log('ne Pomadoras');
+}
+
+console.log('----------------');
+console.log('----------------');
+console.log('----------------');
+console.log('CIKLAI');
+console.log('--');
+console.log('');
+
+let dalinasi3 = 0;
+let dalinasi4 = 0;
+let dalinasi5 = 0;
+
+
+console.log('Ciklas: 0 - 11');
+const cikloSk = 11;
+for (let i=0; i <= cikloSk; i++){
+    if ((i % 3) == 0){
+        dalinasi3++;
+    } 
+}
+console.log(`Cikle 0-11 be liekanos skaičius 3 dalinasi: ${dalinasi3} kartus`);
+
+
+console.log('Ciklas: 8 - 31');
+const cikloSk2 = 31;
+for (let i=8; i <= cikloSk2; i++){
+    if (i != 0 && (i % 3) == 0){
+        dalinasi3++;
+    } 
+}
+console.log(`Cikle 8-31 be liekanos skaičius 3 dalinasi: ${dalinasi3} kartus`);
+
+
+console.log('Ciklas: -18 - 18');
+const cikloSk3 = 18;
+for (let i=-18; i <= cikloSk3; i++){
+    if (i != 0 && (i % 3) == 0){
+        dalinasi3++;
+    } 
+}
+console.log(`Cikle -18-18 be liekanos skaičius 3 dalinasi: ${dalinasi3} kartus`);
+
+
+console.log('----------------');
+console.log('----------------');
+console.log('----------------');
+console.log('Funkcijos');
+console.log('--');
+console.log('');
+
+console.log('Daugyba');
+
+const skaicius1 = 3;
+const skaicius2 = 5;
+function daugybosf(x, y, daugyba){
+    daugyba = skaicius1 * skaicius2;
+    return daugyba;
+}
+console.log(daugybosf(skaicius1,skaicius2));
+
+
+console.log('');
+console.log('--');
+console.log('');
+
+console.log('Skaitmenų kiekis skaičiuje');
+console.log('');
+
+const sk = 'a';
+console.log(`Pateikta reikšmė yra: ${sk}`);
+function kiekisSkaiciuje(x){
+    x = sk;
+    if (!isNaN(x)){
+        return `Pateiktos reikšmės simbolių skaičius yra: ${x.length}`;
+    } else {
+        return 'Pateikta netinkamo tipo reikšmė.';
+    }
+}
+console.log(kiekisSkaiciuje());
+
+console.log('');
+console.log('Didžiausias Skaičius saraše');
+console.log('');
+
+
+
+function didziausiasSkaiciu(){
+
+    const bandymas = [0];
+
+    if (Array.isArray(bandymas)){
+
+        if (bandymas.length > 0) {
+            console.log('tesiame');
+        } else {
+            console.log('Pateiktas sąrašas negali būti tuščias.');
+        }
+    } else {
+    console.log('Pateikta netinkamo tipo reikšmė.');
+    } 
+    
+    // // return;
+}
+console.log(didziausiasSkaiciu());
